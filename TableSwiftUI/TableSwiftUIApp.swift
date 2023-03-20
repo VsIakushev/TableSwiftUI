bd8cfd77468c0ac5ce6970a8dbd14dc28d0ba4ed
Vitalii Iakushev <vsiakushev@gmail.com>
2023-03-20 21:36:45 +0400

    Initial Commit

---
+++ TableSwiftUI/TableSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TableSwiftUIApp.swift
+//  TableSwiftUI
+//
+//  Created by Vitaliy Iakushev on 20.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct TableSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
